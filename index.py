1f157c9d64320ba7a554573c24627030e8cc23a1
07RRJ <rasmusrudko07@gmail.com>
2026-03-20 17:59:00 +0100

    logic is done

--- index.py
+++ index.py
@@ -1,14 +1,19 @@
 from os import system
+from threading import Thread
+from time import sleep
+from msvcrt import getwch
 
 def Cls():
     system("cls")
 
 class Elevator:
-    DIRECTION = "up"
+    DIRECTION = None
     FLOOR = 0
     STOPING_ON_FLOOR = False
+    LIST_UP = []
+    LIST_DOWN = []
 
-    def CheckIfRequestToFloor(self, listUp, listDown):
+    def CheckIfRequestToFloor(self):
         if self.FLOOR == 9:
             self.DIRECTION = "down"
             print("highest floor, going down")
@@ -18,48 +23,55 @@ class Elevator:
             print("lowest floor, going up")
             return
 
-        if self.DIRECTION == "up" and listUp:
+        if self.DIRECTION == "up" and self.LIST_UP:
             return
-        elif self.DIRECTION == "down" and listDown:
+        elif self.DIRECTION == "down" and self.LIST_DOWN:
             return
         else:
             self.DIRECTION = None
         
-        if listUp and self.DIRECTION == None:
+        if self.LIST_UP and self.DIRECTION == None:
             self.DIRECTION = "up"
-        elif listDown and self.DIRECTION == None:
+        elif self.LIST_DOWN and self.DIRECTION == None:
             self.DIRECTION = "down"
 
     def Move(self):
         if self.DIRECTION == "up":
             self.FLOOR += 1
         if self.DIRECTION == "down":
-            elevator.FLOOR -= 1
+            self.FLOOR -= 1
+
+    def On(self):
+        sleep(1)
+        Cls()
+        print(self.FLOOR, self.DIRECTION, self.FLOOR in self.LIST_UP, self.LIST_UP, self.FLOOR in self.LIST_DOWN, self.LIST_DOWN)
+        self.CheckIfRequestToFloor()
+        self.Move()
+        if elevator.FLOOR in elevator.LIST_UP:
+            print("you reached the floor")
+            elevator.LIST_UP.remove(elevator.FLOOR)
+        if elevator.FLOOR in elevator.LIST_DOWN:
+            print("you reached the floor")
+            elevator.LIST_DOWN.remove(elevator.FLOOR)
+        print("enter on floor: ")
+        self.On()
 
 elevator = Elevator()
 
-listUp = []
-listDown = []
-
 floors = [i for i in range(10)]
 
+thread = Thread(target=elevator.On)
+
+thread.start()
+
 while True:
-    print(elevator.FLOOR, elevator.DIRECTION, elevator.FLOOR in listUp, listUp, elevator.FLOOR in listDown, listDown)
-    if elevator.FLOOR in listUp:
-        print("you reached the floor")
-        listUp.remove(elevator.FLOOR)
-    if elevator.FLOOR in listDown:
-        print("you reached the floor")
-        listDown.remove(elevator.FLOOR)
-    elevator.CheckIfRequestToFloor(listUp, listDown)
+    
     try:
-        floorRequest = int(input("enter on floor: "))
+        floorRequest = int(getwch())
     except:
         pass
-    Cls()
     if floorRequest != elevator.FLOOR and floorRequest in floors:
-        if elevator.FLOOR < floorRequest and floorRequest not in listUp:
-            listUp.append(floorRequest)
-        elif elevator.FLOOR > floorRequest and floorRequest not in listDown:
-            listDown.append(floorRequest)
-    elevator.Move()
+        if elevator.FLOOR < floorRequest and floorRequest not in elevator.LIST_UP:
+            elevator.LIST_UP.append(floorRequest)
+        elif elevator.FLOOR > floorRequest and floorRequest not in elevator.LIST_DOWN:
+            elevator.LIST_DOWN.append(floorRequest)
